6d83bf15c58ab5806ca46056f6ed59f6d80a28a9
choyj0920 <choyj0920@naver.com>
2019-10-17 23:13:51 +0900

    ~10717

---
+++ 1_0903/12_1017/12_1017.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+#include "ArrayData.h"
+using namespace std;
+
+void showArray(ArrayData arr) { //여기서도 복사 생성자
+	arr.showData();
+}
+double getArraySum(const ArrayData& arr) {
+	double total = 0;
+	for (int i = 0; i < arr.getUsed(); i++) {
+		total += arr.data[i];
+	}
+	return total;
+}
+
+int main()
+{
+	//cout << "201810754 조윤직\n";
+	//ArrayData arr1(3);
+	//arr1.addElement(1.1);
+	//arr1.addElement(3.1);
+
+	//ArrayData arr2 = arr1.getObject();
+	//ArrayData arr3 = ArrayData(10);
+
+	/*ArrayData arr[] = { 
+		ArrayData(10),ArrayData(20),ArrayData(30) 
+	};*/
+	ArrayData arr(10);
+	ArrayData arr2 = ArrayData::getCopyInstance(arr);
+	system("pause");
+
+
+}
---
+++ 1_0903/midtest__/midtest__.cpp
@@ -0,0 +1,44 @@
+﻿
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    std::cout << "201810754 조윤직!\n";
+}
+
+class User {
+	string id, pw;
+	User(){
+
+	}
+	User(string id, string pw) {
+
+	}
+};
+
+class Ticket{
+	string title;
+	double price;
+	string fname;
+	int** map;
+	User** user;
+	Ticket() {
+
+	}
+	Ticket(string _title, double _price, string _fname) {
+		this->fname = _fname;
+		this->title = _title;
+		this -> price = _price;
+		int height, width;
+		ifstream rin(fname);
+		cin >> width >> height;
+
+	}
+	void showseats() {
+		for(int i=0; i)
+	}
+	void reserve() {
+		
+	}
+};
